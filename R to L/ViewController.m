d4d533183c1ae1b795cadf9962955f93f23268ea
ndot <ndot@kathir-mac.local>
2015-08-22 12:05:56 +0530

    Initial Commit

---
+++ R to L/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  R to L
+//
+//  Created by ndot on 22/08/15.
+//  Copyright (c) 2015 Ktr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ R to LTests/R_to_LTests.m
@@ -0,0 +1,40 @@
+//
+//  R_to_LTests.m
+//  R to LTests
+//
+//  Created by ndot on 22/08/15.
+//  Copyright (c) 2015 Ktr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface R_to_LTests : XCTestCase
+
+@end
+
+@implementation R_to_LTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
